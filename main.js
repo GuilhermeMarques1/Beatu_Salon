c04a6d489187bfe2e2de465fe4fdf378f200b047
GuilhermeMarques1 <g-marques@outlook.com>
2023-08-04 01:53:20 -0300

    refactor: Add shadow for scroll on header

--- main.js
+++ main.js
@@ -16,3 +16,14 @@ for (const item of menuItems) {
     nav.classList.remove("show");
   });
 }
+
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
