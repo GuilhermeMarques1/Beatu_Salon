6b6e0ed30facb43aeedd594f3813a5dccfc0be81
Guilherme Marques <g-marques@outlook.com>
2023-08-06 20:14:31 -0300

    feat: Config testimonials

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener("click", function () {
     nav.classList.toggle("show");
   });
 }
@@ -12,18 +12,30 @@ for (const element of toggle) {
 const menuItems = document.querySelectorAll("#header nav ul li a");
 
 for (const item of menuItems) {
-  item.addEventListener('click', function() {
+  item.addEventListener("click", function () {
     nav.classList.remove("show");
   });
 }
 
 const header = document.querySelector("#header");
-const navHeight = header.offsetHeight
+const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
-  if(window.scrollY >= navHeight) {
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
+});
+
+/* Testimonials carrousel */
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  mousewheel: true,
+  keyboard: true,
 });
